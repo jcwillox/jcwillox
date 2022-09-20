549bd11af2623a32ccfe5a93040229f86f30a9d8
Josh Willox <joshwillox@gmail.com>
2022-09-21 00:11:59 +1000

    Refactor icons to BrandIcon component

---
+++ components/BrandIcon.vue
@@ -0,0 +1,107 @@
+<script setup lang="ts">
+interface Item {
+  icon: string;
+  link: string;
+  props?: Record<string, unknown>;
+}
+
+const items = {
+  python: {
+    icon: "logos:python",
+    link: "https://python.org"
+  },
+  go: {
+    icon: "logos:go",
+    link: "https://golang.org"
+  },
+  fastapi: {
+    icon: "simple-icons:fastapi",
+    link: "https://fastapi.tiangolo.com",
+    props: { style: "color: #009485" }
+  },
+  typescript: {
+    icon: "logos:typescript-icon",
+    link: "https://typescriptlang.org"
+  },
+  vue: {
+    icon: "logos:vue",
+    link: "https://vuejs.org"
+  },
+  vuetifyjs: {
+    icon: "logos:vuetifyjs",
+    link: "https://vuetifyjs.com"
+  },
+  react: {
+    icon: "logos:react",
+    link: "https://reactjs.org"
+  },
+  lit: {
+    icon: "logos:lit-icon",
+    link: "https://lit.dev"
+  },
+  docker: {
+    icon: "logos:docker-icon",
+    link: "https://docker.com"
+  },
+  git: {
+    icon: "logos:git-icon",
+    link: "https://git-scm.com"
+  },
+  express: {
+    icon: "logos:express",
+    link: "https://expressjs.com"
+  },
+  flask: {
+    icon: "logos:flask",
+    link: "https://flask.palletsprojects.com"
+  },
+  powershell: {
+    icon: "vscode-icons:file-type-powershell",
+    link: "https://microsoft.com/powershell"
+  },
+  java: {
+    icon: "logos:java",
+    link: "https://java.com"
+  },
+  mariadb: {
+    icon: "logos:mariadb-icon",
+    link: "https://mariadb.org"
+  },
+  postgresql: {
+    icon: "logos:postgresql",
+    link: "https://postgresql.org"
+  },
+  bash: {
+    icon: "logos:bash-icon",
+    link: "https://gnu.org/software/bash"
+  },
+  c: {
+    icon: "logos:c",
+    link: "https://en.wikipedia.org/wiki/C_(programming_language)"
+  },
+  mongodb: {
+    icon: "logos:mongodb-icon",
+    link: "https://mongodb.com"
+  },
+  scala: {
+    icon: "vscode-icons:file-type-scala",
+    link: "https://scala-lang.org"
+  },
+  "google-cloud": {
+    icon: "logos:google-cloud",
+    link: "https://cloud.google.com"
+  }
+};
+
+const items_: Record<string, Item> = items;
+
+defineProps<{
+  brand: keyof typeof items;
+}>();
+</script>
+
+<template>
+  <NuxtLink :to="items_[brand].link">
+    <Icon :icon="items_[brand].icon" v-bind="items_[brand].props" />
+  </NuxtLink>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -9,47 +9,40 @@ useHead({
     }
   ]
 });
-
-const icons = {
-  "logos:python": "https://python.org",
-  "logos:go": "https://golang.org",
-  "simple-icons:fastapi": {
-    url: "https://fastapi.tiangolo.com",
-    color: "#009485"
-  },
-  "logos:typescript-icon": "https://typescriptlang.org",
-  "logos:vue": "https://vuejs.org",
-  "logos:vuetifyjs": "https://vuetifyjs.com",
-  "logos:react": "https://reactjs.org",
-  // react native
-  "logos:lit-icon": "https://lit.dev",
-  "logos:docker-icon": "https://docker.com",
-  "logos:git-icon": "https://git-scm.com",
-  // logos:express: "https://expressjs.com",
-  // logos:flask: "https://flask.palletsprojects.com",
-  "vscode-icons:file-type-powershell": "https://microsoft.com/powershell",
-  "logos:java": "https://java.com",
-  "logos:mariadb-icon": "https://mariadb.org",
-  "logos:postgresql": "https://postgresql.org",
-  "logos:bash-icon": "https://gnu.org/software/bash"
-};
-
-const secondaryIcons = {
-  "logos:c": "https://en.wikipedia.org/wiki/C_(programming_language)",
-  // assembly
-  "logos:mongodb-icon": "https://www.mongodb.com",
-  "vscode-icons:file-type-scala": "https://www.scala-lang.org",
-  "logos:google-cloud": "https://cloud.google.com"
-};
 </script>
 
 <template>
   <Columns class="mt-8 gap-y-16">
     <ProfileCard />
     <div class="text-center flex justify-center flex-col flex-wrap px-4">
-      <IconGroup :icons="icons" />
-      <hr />
-      <IconGroup :icons="secondaryIcons" />
+      <div class="flex flex-wrap gap-2 justify-center">
+        <BrandIcon brand="python" />
+        <BrandIcon brand="go" />
+        <BrandIcon brand="fastapi" />
+        <BrandIcon brand="typescript" />
+        <BrandIcon brand="vue" />
+        <BrandIcon brand="vuetifyjs" />
+        <BrandIcon brand="react" />
+        <!-- react native -->
+        <BrandIcon brand="lit" />
+        <BrandIcon brand="docker" />
+        <BrandIcon brand="git" />
+        <!-- <BrandIcon brand="express" /> -->
+        <!-- <BrandIcon brand="flask" /> -->
+        <BrandIcon brand="powershell" />
+        <BrandIcon brand="java" />
+        <BrandIcon brand="mariadb" />
+        <BrandIcon brand="postgresql" />
+        <BrandIcon brand="bash" />
+      </div>
+      <hr style="margin-top: 2rem; margin-bottom: 2rem" />
+      <div class="flex flex-wrap gap-2 justify-center">
+        <BrandIcon brand="c" />
+        <!-- assembly -->
+        <BrandIcon brand="mongodb" />
+        <BrandIcon brand="scala" />
+        <BrandIcon brand="google-cloud" />
+      </div>
     </div>
   </Columns>
 </template>
