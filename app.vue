99a6edd145915b4e493e85270d63ba4285d814ea
Josh Willox <joshwillox@gmail.com>
2024-01-06 23:04:43 +1100

    feat: simplify root layout and improve seo

--- app.vue
+++ app.vue
@@ -1,37 +1,43 @@
+<script setup lang="ts">
+useServerHead({
+  link: [
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png",
+    },
+    {
+      rel: "manifest",
+      href: "/site.webmanifest",
+    },
+  ],
+});
+useServerSeoMeta({
+  title: "Joshua Cowie-Willox",
+  ogTitle: "Joshua Cowie-Willox",
+  description: "My personal showcase website",
+  ogDescription: "My personal showcase website",
+  ogImage: "https://avatars.githubusercontent.com/jcwillox",
+});
+</script>
+
 <template>
-  <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-  <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-  <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-  <Link rel="manifest" href="/site.webmanifest" />
-
-  <div class="font-sans">
-    <Navigation />
+  <div class="[--un-default-border-color:#525252] text-white font-sans">
     <NuxtLoadingIndicator />
-
-    <div
-      class="m-auto max-w-screen-lg px-4 pb-8 pt-4 line-height-7 prose prose-gray xl:max-w-screen-xl sm:px-8 dark:prose-invert"
-    >
+    <div class="p-4 pb-12 sm:px-12">
       <NuxtPage />
     </div>
   </div>
 </template>
-
-<style lang="postcss">
-.dark {
-  color-scheme: dark;
-}
-
-.dark body {
-  @apply text-zinc-200 bg-[#0a0a0a];
-}
-
-.light pre {
-  --tw-prose-pre-bg: theme("colors.slate.50");
-}
-
-h2 a,
-h3 a,
-h4 a {
-  @apply no-underline !important;
-}
-</style>
