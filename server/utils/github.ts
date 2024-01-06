ae772ab163cf07df8526872327d5f0084a4ff243
Josh Willox <joshwillox@gmail.com>
2024-01-06 23:03:59 +1100

    feat: extra additional repository information

--- server/utils/github.ts
+++ server/utils/github.ts
@@ -1,16 +1,28 @@
+import { ofetch } from "ofetch";
+import { HOUR_12, HOUR_6 } from "~/constants";
+
 const { GITHUB_TOKEN } = useRuntimeConfig();
 
-export const ghfetch = $fetch.create({
-  baseURL: "https://api.github.com",
-  headers: {
-    ...(GITHUB_TOKEN && { Authorization: `token ${GITHUB_TOKEN}` }),
+export const ghfetch = defineCachedFunction(
+  ofetch.create({
+    baseURL: "https://api.github.com",
+    headers: {
+      ...(GITHUB_TOKEN && { Authorization: `token ${GITHUB_TOKEN}` }),
+    },
+    onResponse({ request, response, options }) {
+      const limit = response.headers.get("X-RateLimit-Limit");
+      const remaining = response.headers.get("X-RateLimit-Remaining");
+      if (limit && remaining) {
+        const pathname = request.toString().replace(options.baseURL + "/", "");
+        console.debug(`gh: ${pathname} (${remaining}/${limit})`);
+      }
+    },
+  }),
+  {
+    group: "gh",
+    name: "fetch",
+    swr: true,
+    maxAge: HOUR_6,
+    staleMaxAge: HOUR_12,
   },
-  onResponse({ request, response, options }) {
-    const limit = response.headers.get("X-RateLimit-Limit");
-    const remaining = response.headers.get("X-RateLimit-Remaining");
-    if (limit && remaining) {
-      const pathname = request.toString().replace(options.baseURL + "/", "");
-      console.debug(`gh: ${pathname} (${remaining}/${limit})`);
-    }
-  },
-});
+);
