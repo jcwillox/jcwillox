83e404ac6480dadee0b0e4c02dacbbc75d026d8e
Josh Willox <joshwillox@gmail.com>
2025-02-13 15:59:16 +1100

    feat: add shortlink redirects

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 import { HOUR_1 } from "./constants";
 
+const ghRaw = (repo: string, path: string) =>
+  `https://github.com/jcwillox/${repo}/raw/main/${path}`;
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@vueuse/nuxt", "@unocss/nuxt", "@nuxt/image", "nuxt-particles"],
@@ -19,6 +22,12 @@ export default defineNuxtConfig({
   },
   routeRules: {
     "/": { isr: HOUR_1 },
+    "/l/syslink": {
+      redirect: ghRaw("system-link", "scripts/install-system-link.sh"),
+    },
+    "/l/syslink-ps1": {
+      redirect: ghRaw("system-link", "scripts/install-system-link.ps1"),
+    },
   },
   devtools: { enabled: true },
   typescript: {
